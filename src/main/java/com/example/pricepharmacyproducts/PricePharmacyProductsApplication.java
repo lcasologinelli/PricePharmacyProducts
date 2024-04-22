6685548683089554c8cdab68825f824cf1c44fd8
GistStuntman <loris.casologinelli97@gmail.com>
2024-04-22 17:18:56 +0200

    Initial commit

---
+++ src/main/java/com/example/pricepharmacyproducts/PricePharmacyProductsApplication.java
@@ -0,0 +1,14 @@
+package com.example.pricepharmacyproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricePharmacyProductsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PricePharmacyProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pricepharmacyproducts/PricePharmacyProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pricepharmacyproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricePharmacyProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
